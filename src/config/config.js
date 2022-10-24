d697825038281a5851c41239329d6878e8b1f4de
TNKKung <tomkabtokom@gmail.com>
2022-10-24 17:12:17 +0700

    add private key from env  in config file

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,21 @@
-module.exports = { port: 4000 };
+const dotenv = require("dotenv");
+const path = require("path");
+const joi = require("joi");
 
+dotenv.config({ path: path.join(__dirname, "../../.env") });
 
+const envSchema = joi
+  .object()
+  .keys({
+    PRIVATE_KEY: joi.string().required(),
+  })
+  .unknown(true);
+const { value: env, error } = envSchema
+  .prefs({ errors: { label: "key" } })
+  .validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+module.exports = { port: 4000, privateKey: env.PRIVATE_KEY };
