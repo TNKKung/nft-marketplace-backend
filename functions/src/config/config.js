e567f1fe4ae2062b1c886ee5de17d7634621ccae
TNattawutK <tomkabtokom@gmail.com>
2023-02-23 01:57:27 +0700

    fix: add contract in config

--- functions/src/config/config.js
+++ functions/src/config/config.js
@@ -23,4 +23,6 @@ module.exports = {
   privateKey: env.PRIVATE_KEY,
   jwtSecretKey: env.JWT_SECRET_KEY,
   moralisApiKey: env.MORALIS_API_KEY,
+  marketplaceContract: "0xE810a1Fa602E71548e3E40E0Db0109fF47A9B7D9",
+  nftContract: "0x985F253fB2F1b47acAAA6fcdc1D00178f7E7B207",
 };
