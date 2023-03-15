131285b9b6ff33e8eedf9206c6e9b26b5318e4a8
TNattawutK <tomkabtokom@gmail.com>
2023-03-15 23:24:08 +0700

    fix: get price & url in api getCollection

--- functions/src/services/collection.service.js
+++ functions/src/services/collection.service.js
@@ -1,7 +1,20 @@
 const { store } = require("../config/firebase");
+const { ethers } = require("ethers");
+const dotenv = require("dotenv");
+const path = require("path");
+const Web3 = require("web3");
+
+const { getProvider } = require("../utils/provider");
+const {
+  privateKey,
+  nftContract,
+  marketplaceContract,
+} = require("../config/config");
 
 const storeCollection = store.collection("Collections");
 const storeNFT = store.collection("NFTs");
+
+dotenv.config({ path: path.join(__dirname, "../../.env") });
 
 const createCollectionService = async (body) => {
   const response = await storeCollection.add({
@@ -34,21 +47,49 @@ const getAllCollectionService = async () => {
 };
 
 const getCollectionByIdService = async (id) => {
-  const collections = await storeCollection.get();
+  const collections = await storeCollection.doc(id).get();
   const NFTs = await storeNFT.get();
-  const returnStore = [];
-  const listNFT = [];
-  NFTs.docs.map((doc) => {
-    if (doc.data().collectionId === id) {
-      listNFT.push(doc.data().tokenId);
-    }
-  });
-  collections.docs.map((doc) => returnStore.push({ ...doc.data(), listNFT }));
-  for (let i = 0; i < returnStore.length; i++) {
-    if (returnStore[i].collectionId === id) {
-      return returnStore[i];
-    }
-  }
+  const provider = getProvider();
+  const web3 = new Web3();
+
+  const signer = new ethers.Wallet(privateKey, provider);
+
+  const abi = ["function tokenURI(uint256 tokenId) view returns (string)"];
+
+  const contract = new ethers.Contract(nftContract, abi, signer);
+
+  const abiMarketplace = [
+    "function priceFromTokenId(uint256 tokenId) view returns (uint256)",
+  ];
+
+  const marketplace = new ethers.Contract(
+    marketplaceContract,
+    abiMarketplace,
+    signer
+  );
+
+  const filterNFTs = NFTs.docs.filter((doc) => doc.data().collectionId === id);
+  const data = await Promise.all(
+    filterNFTs.map(async (doc) => {
+      const result = await contract.functions.tokenURI(doc.data().tokenId);
+      const resultPrice = await marketplace.functions.priceFromTokenId(
+        doc.data().tokenId
+      );
+      const wei = web3.utils.toBN(resultPrice[0]["_hex"]).toString();
+      const eth = ethers.utils.formatEther(wei);
+      return {
+        tokenId: doc.data().tokenId,
+        nameNFT: doc.data().nameNFT,
+        tokenURI: result[0],
+        price: doc.data().statusSale ? eth : "",
+      };
+    })
+  );
+
+  return {
+    ...collections.data(),
+    listNFT: data,
+  };
 };
 
 const getCollectionByOwnerService = async (owner) => {
