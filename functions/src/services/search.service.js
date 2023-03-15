ab3dcee49050f11fb7c38426d593f21e27ed8dc6
TNattawutK <tomkabtokom@gmail.com>
2023-03-16 00:21:49 +0700

    fix: remove comment

--- functions/src/services/search.service.js
+++ functions/src/services/search.service.js
@@ -5,30 +5,6 @@ const getAllSearchListService = async (keyword) => {
   const storeUsers = await store.collection("Users").get();
   const storeCollections = await store.collection("Collections").get();
 
-  // storeNFTs.docs.map((doc) => {
-  //   if (
-  //     doc.data().tokenId.toString().includes(keyword) ||
-  //     doc.data().ownerAddress === keyword ||
-  //     doc.data().nameNFT.includes(keyword)
-  //   ) {
-  //     searchList[1].lists.push(doc.data());
-  //   }
-  // });
-
-  // storeUsers.docs.map((doc) => {
-  //   if (doc.data().address === keyword || doc.data().name.includes(keyword)) {
-  //     searchList[0].lists.push(doc.data());
-  //   }
-  // });
-
-  // storeCollections.docs.map((doc) => {
-  //   if (doc.data().collectionName.includes(keyword)) {
-  //     searchList[2].lists.push(doc.data());
-  //   }
-  // });
-
-  // return searchList;
-
   const filterNFTs = storeNFTs.docs.filter(
     (doc) =>
       doc.data().tokenId.toString().includes(keyword) ||
