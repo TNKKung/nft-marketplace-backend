cbaebe95f1b9e8cd8f63a6b029ca1b73f10ea24a
TNattawutK <tomkabtokom@gmail.com>
2023-04-17 17:59:37 +0700

    fix: fix problem CORS

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,11 @@ const express = require("express");
 const cors = require("cors");
 
 const routes = require("./src/routes");
+import { corsOptions } from "./src/config/allowOrigin";
 
 const app = express();
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.use(cors());
 
---
+++ functions/src/config/allowOrigin.js
@@ -0,0 +1,5 @@
+export const allowedOrigins = ["https://nft-marketplace-frontend.web.app/"];
+
+export const corsOptions = {
+  origin: allowedOrigins,
+};
