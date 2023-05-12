befe0b1b1e9d33b0c5f39396e652981b5d8f009e
TNattawutK <tomkabtokom@gmail.com>
2023-05-13 01:09:58 +0700

    fix: remove moralis api key

--- functions/src/config/config.js
+++ functions/src/config/config.js
@@ -22,7 +22,6 @@ module.exports = {
   port: 4000,
   privateKey: env.PRIVATE_KEY,
   jwtSecretKey: env.JWT_SECRET_KEY,
-  moralisApiKey: env.MORALIS_API_KEY,
   marketplaceContract: "0xE810a1Fa602E71548e3E40E0Db0109fF47A9B7D9",
   nftContract: "0x985F253fB2F1b47acAAA6fcdc1D00178f7E7B207",
 };
